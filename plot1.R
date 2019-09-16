0c4bb6d9b927d9623b90ddd134ebf12e7909ac84
alexchrismi <alexander.christiaan.smit@gmail.com>
2019-09-16 23:11:00 +0200

    fixed error in code

--- plot1.R
+++ plot1.R
@@ -17,66 +17,10 @@ if (!file.exists("household_power_consumption.rds")) {
 dta <- invisible(readRDS("household_power_consumption.rds"))
 par(mfrow = c(1,1))
 
-## Plot 2: Global active power (kilowatts) by day 
-plot(dta$date, dta$Global_active_power, 
-     xlab = "",
-     ylab = "Global Active Power (kilowatts)",
-     type = "l")
-dev.copy(png, file = "plot2.png")
-dev.off()
-
-## Plot 3: Energy submetering by day and by meter
-plot(dta$date, dta$Sub_metering_1,
-     xlab = "",
-     ylab = "Energy sub metering",
-     type = "l")
-lines(dta$date, dta$Sub_metering_2, col = "red")
-lines(dta$date, dta$Sub_metering_3, col = "blue")
-legend("topright",
-       lty = 1, 
-       col = c("black", "red", "blue"),
-       legend = colnames(dta[7:9]))
-dev.copy(png, file = "plot3.png")
-dev.off()
-
-## Plot 4: Two (slightly modified) plots and two new plots all in one panel
-par(mfrow = c(2,2))
-
-## Plot quadrant II
-plot(dta$date, dta$Global_active_power, 
-     xlab = "",
-     ylab = "Global Active Power",
-     type = "l")
-
-## Plot quadrant I: Voltage by day
-plot(dta$date, dta$Voltage, 
-     xlab = "datetime",
-     ylab = "Voltage",
-     type = "l",
-     yaxt = "n")
-lab <- suppressWarnings(data.frame(cbind(sel = c(TRUE, FALSE, FALSE, FALSE), lab = ceiling(min(dta$Volt):floor(max(dta$Volt))))))
-lab <- lab[lab$sel == 1, ][[2]]
-axis(side = 2, at = c(lab))
-
-## Plot quadrant III
-plot(dta$date, dta$Sub_metering_1,
-     xlab = "",
-     ylab = "Energy sub metering",
-     type = "l")
-lines(dta$date, dta$Sub_metering_2, col = "red")
-lines(dta$date, dta$Sub_metering_3, col = "blue")
-
-legend("topright",
-       lty = 1, 
-       col = c("black", "red", "blue"),
-       bty='n',
-       legend = colnames(dta[7:9]))
-
-## Plot quadrant IV: Global re_active power by day
-plot(dta$date, dta$Global_reactive_power,
-     xlab = "datetime",
-     ylab = colnames(dta[4]),
-     type = "l")
-
-dev.copy(png, file = "plot4.png")
+## Plot 1: Histogram of Global Active Power (kilowatts)
+hist(dta$Global_active_power, 
+     main = "Global Active Power",
+     xlab = "Global Active Power (kilowatts)",
+     col = "red")
+dev.copy(png, file = "plot1.png")
 dev.off()
